6673387f6ea4f25fac370d80568efd70c312b4f9
JeEt <ranjeet78827@gmail.com>
2025-02-15 07:04:48 +0000

    Overview of Variables

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 134545
+let accountEmail = "jeet@gmail.com"
+var accountPassword = "1654865"
+accountCity = "Haryana"
+
+// accountId = 461554; It is not allowed 
+
+accountEmail = "jk@gmail.com"
+accountPassword = "254984"
+accountCity = "Bihar"
+
+/*
+   We will not use var because of issue block scope and functional scope 
+*/
+
+// console.log(accountId);
+
+console.table([accountEmail , accountPassword , accountCity])
