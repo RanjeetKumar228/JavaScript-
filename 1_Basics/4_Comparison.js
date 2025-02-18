c16b78434c406ec3dc76d762e05f8a05c42eebb3
JeEt <ranjeet78827@gmail.com>
2025-02-18 00:59:12 +0000

    Comparison

---
+++ 1_Basics/4_Comparison.js
@@ -0,0 +1,34 @@
+console.log(2>1);
+console.log(2==1);
+console.log(2>=1);
+console.log(2<1);
+console.log(2<=1);
+console.log(2!=1);
+//console.log(2=!1);    // Ese nhi likhna
+
+/* Ye sb comparison to simple but problem tb aati hai jb hum koi same data type ki comparison nhi krte */
+
+console.log("2">1)
+console.log("02">1)
+
+//console.log(null>0);
+//console.log(null<0);
+//console.log(null==0);
+//console.log(null>=0);
+/* es trah ke comparison se door rhe */
+
+/*
+  ye jo == hai ya === hai ya > hai ya < hai ya >= in sb alg syntax hai
+   
+  Same hi jb undefined ke sath comparison krte hai to aata hai 
+*/
+
+//console.log(undefined>0);    
+//console.log(undefined<0);      
+//console.log(undefined==0);
+//console.log(undefined>=0);
+/* es trah ke comparison se door rhe */
+
+//  ===   (Strict Check)
+
+console.log("2"===2)
