1b3040966c17e4f8e3b93ba4f604687c65ff10e2
JeEt <ranjeet78827@gmail.com>
2025-02-18 00:23:10 +0000

    Operation discussed

--- 01_Basics/03_ConversionOperation.js
+++ 01_Basics/03_ConversionOperation.js
@@ -1,11 +1,11 @@
 let score = "JeEt"
 
-console.log(typeof score);
-console.log(typeof (score));  // As a method
+//console.log(typeof score);
+//console.log(typeof (score));  // As a method
 
 let valueInNumber = Number (score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);          // NaN => Not A Number 
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);          // NaN => Not A Number 
 
 
 //             **** NOTE ****  
@@ -17,18 +17,81 @@ console.log(valueInNumber);          // NaN => Not A Number
 
 let isLoggedin = "JeEt"
 let booleanNumber = Boolean(isLoggedin)
-console.log(booleanNumber);
+//console.log(booleanNumber);
 
 
 //             **** NOTE ****  
           
 //       1 => true;
 //       0 => false;
-//       "" => true
+//       "" => false
 //       "JeEt" => true
 
 
 let sum = 99 
 let stringNumber = String(sum)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
+
+// ********************  OPERATIONS   ***********************
+
+let value = 228
+//let negValue = -value
+//console.log(negValue);
+
+//console.log(3+3);
+//console.log(3-3);
+//console.log(3*3);
+//console.log(3/3);
+//console.log(3%3);
+
+let Num1 = 8
+let Num2 = 10
+let Sum = Num1 + Num2
+//console.log(Sum);
+
+let Str1 = "Hello"
+let Str2 = " JeEt"
+let Str3 = Str1 + Str2
+//console.log(Str3);
+
+//console.log("1" + 2)           
+//console.log(1 + "2");             
+//console.log("1" + 2 + 3);       
+//console.log(1 + 2 + "3");      
+
+/*
+NOTE ==> Agr first String hai to sare string mein Add hoga , agr last mein
+string  hai to pehle sare add ho jayenge phir string hoga 
+*/
+
+
+// console.log(3 + 4 - 2 * 7 / 3); 
+
+/*
+
+ Es trah ke complicated trike se na likhe , isko parentheses mein likhiye kuchh es kdr
+
+ console.log( (3 + 4) - 2 * 7 / 3);
+
+*/ 
+
+//console.log(+true);
+//console.log(+"");
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter);
+++gameCounter
+console.log(gameCounter);
+
+
+
+
+
+
+
+
+
+
+
