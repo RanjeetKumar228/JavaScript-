d089c99d6912aa11d1126f17162368cf53b8a0ac
JeEt <ranjeet78827@gmail.com>
2025-02-18 00:29:42 +0000

    Data types

---
+++ 1_Basics/2_DataTypes.js
@@ -0,0 +1,36 @@
+"use strict" // treat all Js as newer version
+
+// alert(4 + 4)   *** Because we are using node.js ***
+
+
+console.log( 5 + 
+    5) // In programming code readability is most important 
+
+console.log("JeEt")
+
+
+let name = "JeEt"
+let age = 20
+let isLoggedin = false
+
+//             Names of data type
+
+//        *** Primitive data types **
+
+//        1.  numer => 2 to the power 53
+//        2.  bigInt
+//        3.  String => " "
+//        4.  boolean => true / false
+//        5.  null =>standalone value
+//        6.  unrefined 
+//        7.  symbol => unique
+
+//        *** Object *** 
+
+console.log(typeof "JeEt")
+
+console.log(typeof age)
+
+console.log(typeof null);        // object
+
+console.log( typeof undefined);  // undefined
