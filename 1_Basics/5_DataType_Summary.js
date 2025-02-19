036eb281d1261d8562c7279652fb1c4fc5f9f6f3
JeEt <ranjeet78827@gmail.com>
2025-02-19 00:20:14 +0000

    Data Types ( Notes )

---
+++ 1_Basics/5_DataType_Summary.js
@@ -0,0 +1,16 @@
+//     Que => Kis trah data ko memory mien rkha jata hai aur isko access  
+//            kese kia ja skta hai ?
+//     Ans =>  Data ko memory mien rkha jata hai aur isko access  
+//            kese kia ja skta hai uske Basis pr do trike hai 
+
+
+// ********** Primitive **********
+
+//   7 types : String , Boolean , null , undefined, symbol , BigInt
+
+
+// ********** Reference (Non primitive) **********
+
+//    1. Array
+//    2. Object         ( Object is most importen in java script)
+//    3. functions
