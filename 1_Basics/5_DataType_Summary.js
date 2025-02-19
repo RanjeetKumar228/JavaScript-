e97b65aaa12b2f6c9ffc938295e0b6344abd6905
JeEt <ranjeet78827@gmail.com>
2025-02-19 07:16:05 +0000

    Data type ( Summary )

--- 1_Basics/5_DataType_Summary.js
+++ 1_Basics/5_DataType_Summary.js
@@ -8,9 +8,44 @@
 
 //   7 types : String , Boolean , null , undefined, symbol , BigInt
 
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedin = false
+const Temp = null
+let useEmail;
+
+const id = Symbol("123")
+const idNum = Symbol("123")
+//console.log(id === idNum)
+
+const BigInt = 12132135156156165165132132132
+//console.log(BigInt)
 
 // ********** Reference (Non primitive) **********
 
 //    1. Array
 //    2. Object         ( Object is most importen in java script)
 //    3. functions
+
+
+// Array
+
+const jeet= ["Ranjeet","JK","RK"]
+//console.log(jeet)
+
+// Object
+
+const myObj ={
+    Name : "Ranjeet",
+    age : 21
+}
+//console.log(myObj)
+
+// Function
+
+const myFunction = function(){
+    console.log("Hello JeEt")
+}
+
+console.log(typeof idNum)
