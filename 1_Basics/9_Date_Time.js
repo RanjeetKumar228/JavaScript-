2f50175073b0a77c438ec9a0eb0047214a219281
JeEt <ranjeet78827@gmail.com>
2025-02-26 10:30:51 +0000

    Date and Time

--- 1_Basics/9_Date_Time.js
+++ 1_Basics/9_Date_Time.js
@@ -16,6 +16,23 @@ let createDate = new Date("1-28-2024")
 //console.log(createDate.toLocaleString())
 
 let myTimestamp = Date.now()
-console.log(myTimestamp);
-console.log(createDate.getTime());
 
+console.log(myTimestamp);
+//console.log(typeof myTimestamp)   // number
+
+console.log(createDate.getTime());   
+// hour mein ans aaya hai agr isko second mein convert krna hai to 1000 se divide kr dijiye 
+
+console.log(Math.round((Date.now()/1000)))   // second
+
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getMonth()+1);
+console.log(newDate.getDay());
+console.log(newDate.getFullYear());
+
+// console.log(`${newDate.getFullYear()} Month is ${newDate.getMonth()}`);
+
+newDate.toLocaleString('default',{
+   weekday: "long",
+})
