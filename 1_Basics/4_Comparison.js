d9b737f0175ee821f7b286acc969dda119e18eac
JeEt <ranjeet78827@gmail.com>
2025-02-26 00:32:05 +0000

    Comparison

--- 1_Basics/4_Comparison.js
+++ 1_Basics/4_Comparison.js
@@ -1,34 +1,45 @@
-console.log(2>1);
-console.log(2==1);
-console.log(2>=1);
-console.log(2<1);
-console.log(2<=1);
-console.log(2!=1);
+// console.log(2>1);
+// console.log(2==1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2<=1);
+// console.log(2!=1);
 //console.log(2=!1);    // Ese nhi likhna
 
 /* Ye sb comparison to simple but problem tb aati hai jb hum koi same data type ki comparison nhi krte */
 
-console.log("2">1)
-console.log("02">1)
+// console.log("2">3)
+// console.log(typeof("2">3))  // boolean
+// console.log("02">1)
+// console.log(typeof("02">1))  // boolean
 
-//console.log(null>0);
-//console.log(null<0);
-//console.log(null==0);
-//console.log(null>=0);
+// console.log(null>0);     // false 
+// console.log(null<0);     // false      
+// console.log(null==0);    // false 
+// console.log(null===0);   // false 
+// console.log(null>=0);    // true
+// console.log(null<=0);    // true
+
+// console.log(0 > 0);
+
 /* es trah ke comparison se door rhe */
 
 /*
-  ye jo == hai ya === hai ya > hai ya < hai ya >= in sb alg syntax hai
+  == , === ye dono operator java script mein null ko number mein convert nhi  krte hai esiliye jb null>=0 kia to true output aaya
+
+  > , < , >= , <= in sb ka jb java script mein  use hota hai to , null ko number mein converts kia jata hai aur , 0 ki treat krte hai null ko.
    
   Same hi jb undefined ke sath comparison krte hai to aata hai 
 */
 
-//console.log(undefined>0);    
-//console.log(undefined<0);      
-//console.log(undefined==0);
-//console.log(undefined>=0);
+// console.log(undefined>0);    
+// console.log(undefined<0);      
+// console.log(undefined==0);
+// console.log(undefined>=0);
 /* es trah ke comparison se door rhe */
 
 //  ===   (Strict Check)
 
-console.log("2"===2)
+ console.log("2"===2)  // (Strict Check)  false
+
+ console.log("2"==2)   // value check , so this is true 
