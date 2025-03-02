cd4ae6eab43ec1eefe339cb849d7272460780530
JeEt <ranjeet78827@gmail.com>
2025-03-02 10:31:02 +0000

    Object

---
+++ 2_Basics/4_Object.js
@@ -0,0 +1,30 @@
+// const tinder_user = new Object()
+
+const tinder_user = {}
+
+ tinder_id = '123abb'
+ tinder_name = 'Jeet'
+ tinder_isLoggedin = false
+
+// console.log(tinder);
+
+const regular_user = {
+    email:'ranjit123@gmil.com',
+    full_name:{
+        user_name:{
+            first_name:'jeet',
+            last_name: 'Kumar',
+        }
+    }
+}
+
+// console.log(regular_user.full_name.user_name);
+
+const obj1 = {1: 'A', 2: 'B'}
+const obj2 = {3: 'C', 4: 'D'}
+
+// const obj3 = {obj1, obj2}
+
+const obj3 = Object.assign()
+console.log(obj3);
+
