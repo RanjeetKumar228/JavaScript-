6dfa8a5605136840128e18eae9589d075991f3eb
JeEt <ranjeet78827@gmail.com>
2025-03-02 13:48:14 +0000

    Object

--- 2_Basics/4_Object.js
+++ 2_Basics/4_Object.js
@@ -1,30 +1,39 @@
-// const tinder_user = new Object()
+// const tinder_user = new Object()    // ye sigleton hai 
 
-const tinder_user = {}
+const tinder_user  = {}                // ye singleton nhi hai 
 
- tinder_id = '123abb'
- tinder_name = 'Jeet'
- tinder_isLoggedin = false
+tinder_user.id = '123abc'
+tinder_user.email = 'ranjeet@gmail.com'
+tinder_user.isLoggedIn = false;
 
-// console.log(tinder);
+// console.log(tinder_user);
 
-const regular_user = {
-    email:'ranjit123@gmil.com',
-    full_name:{
+const regularUser = {
+    id : '123',
+    email : 'jeet123@gmail.com',
+    full_nmae :{                       // Object ke ander object ho skta 
         user_name:{
-            first_name:'jeet',
-            last_name: 'Kumar',
+            first_name : 'JEET',
+            last_name : 'KUMAR',
         }
     }
 }
 
-// console.log(regular_user.full_name.user_name);
+// console.log(regularUser.full_nmae.user_name)
 
-const obj1 = {1: 'A', 2: 'B'}
-const obj2 = {3: 'C', 4: 'D'}
 
-// const obj3 = {obj1, obj2}
+// Ab hum dekhte hai ki object ko combine kese kre 
 
-const obj3 = Object.assign()
-console.log(obj3);
+const obj1 = {1: 'R', 2: 'A', 3: 'N', 4: 'J'}
+const obj2 = {5: 'E', 6: 'E', 7: 'T'}
+const obj4 = {6: 'K', 7: 'U', 8: 'M', 9: 'A', 10: 'R'}
+// const obj3 = {obj1,obj2} 
+ /*
+   humne array bhi ese si combine kia tha to ek hi array mein do array aa gye the ese hi yahha ek hi object mein do object aa gye hai .Array mein mein isko thik krne ke liye 
+   concat or spread ka use kia tha same yaha krenge but iska name alg hai.
+*/
 
+// const combine_obj = Object.assign({},obj1,obj2)  // ye ho gya hmara .assign ke trike se
+
+const combine_obj = {...obj1 , ...obj2 , ...obj4}  // spread ke trike se
+console.log(combine_obj)
