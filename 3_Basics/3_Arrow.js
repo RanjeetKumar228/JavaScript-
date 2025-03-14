ff6f969722b20092991913cdd20f13fdcf508308
Ranjeet <ranjeet78827@gmail.com>
2025-03-14 01:54:21 +0000

    this and arrow

--- 3_Basics/3_Arrow.js
+++ 3_Basics/3_Arrow.js
@@ -1,17 +1,17 @@
-//Arrow function smjne ke liye hme this ko smjna pdega aur this ko smjne ke liye thora object ko bhi smjna pdega.
+//   Arrow function smjne ke liye hme this ko smjna pdega aur this ko smjne  
+//   ke liye thora object ko bhi smjna pdega.
 
 const user = {
     userName : "Ranjeet",
     price : 199,
- 
     welcomeMsg : function (){
     console.log(`${this.userName} , Welcome to website`);
 // current context ko access krne ke liye this ka use krte hai
 
-//     console.log(this)
-    }
-    
+       console.log(this)
+    }  
 } 
+
 // user.welcomeMsg()
 // user.userName = 'Jeet'
 // user.welcomeMsg()
@@ -26,11 +26,15 @@ console.log(this)    // {} emty object ko reffer krta hai
 // }
 // jeet()
 
-// Jb humne object ke ander this ka use kiya tha to use ho rha tha pr function ke andr nhi ho sath jb hmne function ke ander this ko print krwaya to kafi kuchh dekhne ko miala
+/*
+  Jb humne object ke ander this ka use kiya tha to use ho rha tha pr function ke andr nhi ho sath jb hmne function ke ander this ko print krwaya to kafi kuchh dekhne ko miala
+/*
 
-// sikhne ko kya mila this object ke ander hi kam krta hai.
+/* sikhne ko kya mila this object ke ander hi kam krta hai. */
 
-// Ab dekhte hai function ko aur kitne trike se likh skte hai , pehle trika to uper dekh lia ab dusra dekhte hai
+/*
+  Ab dekhte hai function ko aur kitne trike se likh skte hai , pehla trika to uper dekh lia ab dusra dekhte hai
+*/
 
 // isko ek variable mein store krte hai
 
@@ -48,16 +52,18 @@ console.log(this)    // {} emty object ko reffer krta hai
 // }
 // new3Fun()
 
-// Ab discuss krte hai pure arrow function
+/*     Ab discuss krte hai pure arrow function    */
 
-// Syntax   () => { }
+/*    ******   Syntax   () => { }   ******   */
 
 // const AddTwoNum = (num1,num2) => {
 //      return num1+num2;
 // }
 // console.log((AddTwoNum(4,5)));
 
-// eska dusra trika hai implesit, usme hum {} ko hta ke ()parentheses lgate hai aur important baat usme hme return likhne jrurt nhi pdti.
+/*
+  eska dusra trika hai implicit, usme hum {} ko hta ke ()parentheses lgate hai aur important baat usme hme return likhne jrurt nhi pdti.
+*/
 
 // const AddTwoNum = (num1,num2) => (num1+num2);
 
