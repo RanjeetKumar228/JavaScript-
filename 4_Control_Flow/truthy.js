39b7dcfb2621e534f6a64581d962dfb8b833e7e6
Ranjeet <ranjeet78827@gmail.com>
2025-03-15 11:40:45 +0000

    Control flow codes

--- 4_Control_Flow/truthy.js
+++ 4_Control_Flow/truthy.js
@@ -1,4 +1,4 @@
-// truthy value mtb vo man keta hai ki aapki vaue shi hai
+/*   truthy value mtb vo man leta hai ki aapki value shi hai    */
 
 const username = []
 if(username){
@@ -8,30 +8,49 @@ if(username){
    
 }
 
-//Falsy value
+/*     *****  Falsy value  *****     */
 
-// false , 0 , -0 , "" , BigInt 0n , null , undefined , NAN
+/*  false , 0 , -0 , "" , BigInt 0n , null , undefined , NAN     */
 
-// truthy value
+/*       *****    truthy value    *****        */
 
-// "False" , " " , "0" , {} , [] , function(){}
+/*    "False" , " " , "0" , {} , [] , function(){}    */
 
-// Agr ye check krna pd jaye ki array ya object empty hai ya nhi to kese krenge 
+/* 
+  Agr ye check krna pd jaye ki array ya object empty hai ya nhi to kese krenge.
 
-// Array check krne ka trika
+  Array check krne ka trika
+*/
 
 if(username.length === 0){
     console.log("Array");        //Array
     
 }
 
-//Object check krne ka trika
+/*     *****  Object check krne ka trika  *****   */
 
 const checkObject = {}
 if(Object.keys(checkObject).length === 0){
-    console.log("Object is empty");           //Object is empty
+   // console.log("Object is empty");           //Object is empty
     
 }
 
 
 /*        Nullish Coalecsing Operator (??) : null , undefind       */
+
+let val ;
+// val = 10;
+// val = 5 ?? 10                 // 5
+// val = null ?? 10             // 10
+// val = undefined ?? 10       // 10
+val = null ?? 10 ?? 20
+//console.log(val)
+
+
+/*   *****  Terniary operator  *****     */
+
+// condition ? true : false
+
+let age = 10
+age>18 ? console.log("ADULT"): console.log("Child")
+ 
