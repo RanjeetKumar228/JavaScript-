718ef0c9c27212e6d9f5c059f37c84e3863f3660
Ranjeet <ranjeet78827@gmail.com>
2025-03-29 13:59:46 +0000

    1_Project

--- 7_Projects/1_Project/script.js
+++ 7_Projects/1_Project/script.js
@@ -1,12 +1,39 @@
 const buttons = document.querySelectorAll('.button')
 const body = document.querySelector('body')
 
-buttons.forEach(function(button){
-console.log(button);
-buttons.addEventListener('click',function(event){
-    console.log(event)
-    console.log(event.target);
-    
+buttons.forEach((button)=>{
+    console.log(button);
+    button.addEventListener('click',(event)=>{
+            console.log(event)
+            console.log(event.target);
+                if(event.target.id === "black"){
+                    body.style.backgroundColor = event.target.id
+                }
 
-    });  
-});
+                if(event.target.id === "black"){
+                    body.style.backgroundColor = event.target.id
+                }
+
+                if(event.target.id === "yellow"){
+                    body.style.backgroundColor = event.target.id
+                }
+
+                if(event.target.id === "blue"){
+                    body.style.backgroundColor = event.target.id
+                }
+
+                if(event.target.id === "purple"){
+                    body.style.backgroundColor = event.target.id
+                }
+
+                if(event.target.id === "pink"){
+                    body.style.backgroundColor = event.target.id
+                }
+
+    })
+    
+})
+
+
+
+
