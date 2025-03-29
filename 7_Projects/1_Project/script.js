8f0d9248079ef34f33eb8ae1364f56a217a8f0de
Ranjeet <ranjeet78827@gmail.com>
2025-03-29 12:36:25 +0000

    1_Project

---
+++ 7_Projects/1_Project/script.js
@@ -0,0 +1,12 @@
+const buttons = document.querySelectorAll('.button')
+const body = document.querySelector('body')
+
+buttons.forEach(function(button){
+console.log(button);
+buttons.addEventListener('click',function(event){
+    console.log(event)
+    console.log(event.target);
+    
+
+    });  
+});
