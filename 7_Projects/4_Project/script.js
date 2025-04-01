39dbd192bce612d6f6d1bcb9642a5248c42c9cf0
Ranjeet <ranjeet78827@gmail.com>
2025-04-01 12:36:12 +0000

    Project 4

---
+++ 7_Projects/4_Project/script.js
@@ -0,0 +1,39 @@
+const randomNumber = Math.round(Math.random()*100+1)
+console.log(randomNumber);
+
+
+const userInput = document.getElementById("guessField").value
+const submit = document.getElementById("subt")
+const guessDlot = document.querySelector(".guesses")
+const remaining = document.querySelector(".lastResult")
+const p = document.querySelector(".lowOrHi")
+const startOver = document.querySelector(".resultParas")
+
+
+
+function valiDate(guesses){
+    //
+};
+
+function checkNumber(guesses){
+    //
+};
+
+function displayGuess(guesses){
+    //
+};
+
+function displayMessage(msg){
+    //
+};
+
+function endGame(eG){
+    //
+};
+
+function startGame(sG){
+    //
+};
+
+
+
